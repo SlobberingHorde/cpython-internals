b98e8616c7371ad5663f56e930b8717e88695058
Jerry Hoerig <jhoerig@crossriver.com>
2024-04-03 16:19:12 -0400

    Simplify the setting of the host user
    Cleaned it up so it isn't hardcoded to UID/GID, they are now retrieved from
    the host system in the build.sh and passed as build args. Should work for
    both Mac and Linux this way.

--- build.sh
+++ build.sh
@@ -1,16 +1,9 @@
 
-export HOST_UID=1000
-export HOST_USER=jhoerig
-export HOST_GID=1000
-export HOST_GROUP_NAME=jhoerig
-export CLI_HOME=/home/jhoerig/
-
+export HOST_UID=$(id -u)
+export HOST_GID=$(id -g)
 
 docker build \
     -t cpython-internals:1.0.0 \
     --build-arg HOST_UID \
-    --build-arg HOST_USER \
     --build-arg HOST_GID \
-    --build-arg HOST_GROUP_NAME \
-    --build-arg CLI_HOME \
     .
